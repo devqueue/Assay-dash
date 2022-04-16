31c887e5e959ef0d2c06b2f7283d8fdd376abb00
devqueue <haziq.sayyed@gmail.com>
2022-04-16 13:35:57 +0530

    home configured

--- apps/home/models.py
+++ apps/home/models.py
@@ -4,7 +4,126 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.db import models
-from django.contrib.auth.models import User
-
+import datetime
 # Create your models here.
 
+
+def current_year():
+    return datetime.date.today().year
+
+
+class Samples(models.Model):
+    AssayID = models.CharField(primary_key=True, max_length=50)
+    MachineID = models.CharField(max_length=100)
+    Assay = models.CharField(max_length=100)
+    January = models.FloatField()
+    February = models.FloatField()
+    March = models.FloatField()
+    April = models.FloatField()
+    May = models.FloatField()
+    June = models.FloatField()
+    July = models.FloatField()
+    August = models.FloatField()
+    September = models.FloatField()
+    October = models.FloatField()
+    November = models.FloatField()
+    December = models.FloatField()
+    Year = models.IntegerField(default=current_year)
+    class Meta:
+        abstract = True
+
+
+class Utilization(models.Model):
+    AssayID = models.CharField(primary_key=True, max_length=50)
+    MachineID = models.CharField(max_length=100)
+    Assay = models.CharField(max_length=100)
+    January = models.FloatField()
+    February = models.FloatField()
+    March = models.FloatField()
+    April = models.FloatField()
+    May = models.FloatField()
+    June = models.FloatField()
+    July = models.FloatField()
+    August = models.FloatField()
+    September = models.FloatField()
+    October = models.FloatField()
+    November = models.FloatField()
+    December = models.FloatField()
+    Year = models.IntegerField(default=current_year)
+    class Meta:
+        abstract = True
+
+
+class Revenue(models.Model):
+    AssayID = models.CharField(primary_key=True, max_length=50)
+    MachineID = models.CharField(max_length=100)
+    Assay = models.CharField(max_length=100)
+    January = models.FloatField()
+    February = models.FloatField()
+    March = models.FloatField()
+    April = models.FloatField()
+    May = models.FloatField()
+    June = models.FloatField()
+    July = models.FloatField()
+    August = models.FloatField()
+    September = models.FloatField()
+    October = models.FloatField()
+    November = models.FloatField()
+    December = models.FloatField()
+    Year = models.IntegerField(default=current_year)
+    class Meta:
+        abstract = True
+
+
+class MissedRevenue(models.Model):
+    AssayID = models.CharField(primary_key=True, max_length=50)
+    MachineID = models.CharField(max_length=100)
+    Assay = models.CharField(max_length=100)
+    January = models.FloatField()
+    February = models.FloatField()
+    March = models.FloatField()
+    April = models.FloatField()
+    May = models.FloatField()
+    June = models.FloatField()
+    July = models.FloatField()
+    August = models.FloatField()
+    September = models.FloatField()
+    October = models.FloatField()
+    November = models.FloatField()
+    December = models.FloatField()
+    Year = models.IntegerField(default=current_year)
+    class Meta:
+        abstract = True
+
+
+class stats(models.Model):
+    AssayID = models.CharField(primary_key=True, max_length=50)
+    MachineID = models.CharField(max_length=100)
+    FullCapacity = models.FloatField()
+    RunTime = models.FloatField()
+    Price = models.FloatField()
+    Maintenance = models.FloatField()
+    class Meta:
+        abstract = True
+
+
+class monthlystats(models.Model):
+    AssayID = models.CharField(primary_key=True, max_length=50)
+    MachineID = models.CharField(max_length=100)
+    MaxMonthlyhours = models.FloatField()
+    MaxMonthlyRevenue = models.FloatField()
+    MaxMonthSamples = models.FloatField()
+    class Meta:
+        abstract = True
+
+
+class Csv(models.Model):
+    file_ID = models.AutoField(primary_key=True)
+    file_name = models.FileField(upload_to='upload')
+    uploaded = models.DateTimeField(auto_now_add=True)
+    activated = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"File: {self.file_name}"
+    class Meta:
+        abstract = True
--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,13 +5,15 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
+from django.urls import path, include
 
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-
+    path('lab1/', include('apps.lab1.urls')),
+    path('lab2/', include('apps.lab2.urls')),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
-]
+] 
