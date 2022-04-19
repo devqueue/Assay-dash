58c5adce33c9bd3277b1a64b4702ce72ed8e4549
devqueue <haziq.sayyed@gmail.com>
2022-04-19 15:05:32 +0530

    static files served in dev server

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include  # add this
 
 urlpatterns = [
@@ -11,3 +13,9 @@ urlpatterns = [
     path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("", include("apps.home.urls"))             # UI Kits Html files
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_URL,
+    )
