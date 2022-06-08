f5c92a5d260e850e71ba8ea01e8c8e2f0d59ecf4
devqueue <haziq.sayyed@gmail.com>
2022-06-08 10:34:11 +0530

    models upated for machines

--- apps/lab3/models.py
+++ apps/lab3/models.py
@@ -4,26 +4,27 @@ from apps.home.models import Samples, Utilization, Revenue, MissedRevenue, stats
 # Create your models here.
 
 class Samples(Samples):
-    pass
+    AssayID = None
+    MachineID = models.CharField(primary_key=True, max_length=100)
 
 class Utilization(Utilization):
-    AssayID = models.CharField(max_length=50)
+    AssayID = None
     MachineID = models.CharField(primary_key=True, max_length=100)
 
 class Revenue(Revenue):
-    AssayID = models.CharField(max_length=50)
+    AssayID = None
     MachineID = models.CharField(primary_key=True, max_length=100)
 
 class MissedRevenue(MissedRevenue):
-    AssayID = models.CharField(max_length=50)
+    AssayID = None
     MachineID = models.CharField(primary_key=True, max_length=100)
 
 class stats(stats):
-    AssayID = models.CharField(max_length=50)
+    AssayID = None
     MachineID = models.CharField(primary_key=True, max_length=100)
 
 class monthlystats(monthlystats):
-    AssayID = models.CharField(max_length=50)
+    AssayID = None
     MachineID = models.CharField(primary_key=True, max_length=100)
 
 class Csv(Csv):
