a4ca327f1fc8be791dc85931fd3f29d7e62b8822
devqueue <haziq.sayyed@gmail.com>
2022-06-18 18:34:32 +0530

    models registered in admin

--- apps/lab3/admin.py
+++ apps/lab3/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Samples, Revenue, Utilization, stats, monthlystats, MissedRevenue, Csv
-
+from .models import SamplesMachine, RevenueMachine, UtilizationMachine, statsMachine, monthlystatsMachine, MissedRevenueMachine, SamplesAssay, UtilizationAssay, RevenueAssay, statsAssay, monthlystatsAssay, MissedRevenueAssay, Csv
 # Register your models here.
-admin.site.register([Samples, Revenue, Utilization,
-                    stats, monthlystats, MissedRevenue, Csv])
+admin.site.register([SamplesMachine, RevenueMachine, UtilizationMachine,
+                    statsMachine, monthlystatsMachine, MissedRevenueMachine, SamplesAssay,
+                    UtilizationAssay, RevenueAssay, statsAssay,
+                    monthlystatsAssay, MissedRevenueAssay, Csv])
